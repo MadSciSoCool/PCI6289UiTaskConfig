d6ba74409df2372e5da345251fb0701edf4d5755
Pan Jiahe <expanjiahe@gmail.com>
2019-06-17 21:14:23 +0800

    delete test scripts

--- test_scripts/ao_func_gen.py
+++
@@ -1,7 +0,0 @@
-import nidaqmx
-from nidaqmx.constants import FuncGenType
-
-
-with nidaqmx.Task() as task:
-    task.ao_channels.add_ao_func_gen_chan('Dev1/ao0', type=FuncGenType.SINE, freq=1000, amplitude=1.0)
-    task.start()
--- test_scripts/check_for_terminals.py
+++
@@ -1,7 +0,0 @@
-import nidaqmx.system
-
-
-system = nidaqmx.system.System.local()
-device = system.devices['Dev1']
-for terminal in device.terminals:
-    print(terminal)
